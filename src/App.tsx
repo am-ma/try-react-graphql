98f95eed9b4e953fd2b4e7ef293ab97e5cf9cd13
am-ma <mitsunaga.ayumi@vega-c.com>
2019-08-19 16:39:34 +0900

    :sparkles: add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, lazy, Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import client from './graphql-client';
@@ -12,13 +12,25 @@ const App: FunctionComponent = () => {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route path="/repository" component={Repository} />
-            <Route path="/" component={Home} />
-            <Redirect to="/" />
-          </Switch>
-        </Suspense>
+        <header>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/repository">Repository</Link>
+            </li>
+          </ul>
+        </header>
+        <article>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/repository" component={Repository} />
+              <Route path="/" component={Home} />
+              <Redirect to="/" />
+            </Switch>
+          </Suspense>
+        </article>
       </BrowserRouter>
     </ApolloProvider>
   );
