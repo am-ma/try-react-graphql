2e2de6021ff18ee9cc7c9e4015eb13bbb231ed0f
am-ma <mitsunaga.ayumi@vega-c.com>
2019-08-19 17:36:18 +0900

    :sparkles: toggle star

--- src/services/toggleStar.ts
+++ src/services/toggleStar.ts
@@ -1,21 +1,55 @@
 import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 
 const CURRENT_REPOSITORY_QUERY = gql`
   query CurrentRepository {
     repository(name: "try-react-graphql", owner: "am-ma") {
+      id
       name
       url
       viewerHasStarred
     }
   }
 `;
+
+const ADD_STAR = gql`
+  mutation AddStar($input: AddStarInput!) {
+    addStar(input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const REMOVE_STAR = gql`
+  mutation RemoveStar($input: RemoveStarInput!) {
+    removeStar(input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
 const fetchCurrentRepository = () => {
   return useQuery(CURRENT_REPOSITORY_QUERY);
 };
 
+const addStar = () => {
+  return useMutation(ADD_STAR);
+};
+
+const removeStar = () => {
+  return useMutation(REMOVE_STAR);
+};
+
 const toggleStar = {
   fetchCurrentRepository,
+  addStar,
+  removeStar,
 };
 
 export default toggleStar;
--- src/views/containers/ToggleStar.tsx
+++ src/views/containers/ToggleStar.tsx
@@ -5,12 +5,27 @@ import toggleStar from '~/services/toggleStar';
 type Props = {};
 const ToggleStar: FunctionComponent<Props> = () => {
   const { loading, data, error } = toggleStar.fetchCurrentRepository();
+  const [addStar] = toggleStar.addStar();
+  const [removeStar] = toggleStar.removeStar();
 
   if (loading) return <p>loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   const currentRepos = data.repository;
   if (!currentRepos) return <p>current reppsitory not defined</p>;
+
+  let hasStarred = currentRepos.viewerHasStarred;
+
+  const onClickToggleButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const variables = { input: { starrableId: currentRepos.id } };
+
+    if (currentRepos.viewerHasStarred) {
+      removeStar({ variables });
+    } else {
+      addStar({ variables });
+    }
+  };
 
   return (
     <>
@@ -20,7 +35,9 @@ const ToggleStar: FunctionComponent<Props> = () => {
           {currentRepos.name}
         </a>
       </h3>
-      <button type="button">{currentRepos.viewerHasStarred ? 'remove' : 'add'}</button>
+      <button type="button" onClick={onClickToggleButton}>
+        {hasStarred ? 'remove' : 'add'}
+      </button>
     </>
   );
 };
